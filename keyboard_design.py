b6ac28ef15b107b1441973c55b7010cc1322264d
Silicrex <silicrex@gmail.com>
2021-11-16 19:36:41 -0500

    First commit

---
+++ keyboard_design.py
@@ -0,0 +1,55 @@
+from cv2 import cv2
+import numpy  # Button transparency
+import cvzone  # Button corners
+
+
+class Button:
+    def __init__(self, pos, text, size=(85, 85)):
+        self.pos = pos
+        self.text = text
+        self.size = size
+
+
+def create_keyboard_keys():
+    key_layout = {'top': ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                  'middle': ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+                  'bottom': ['Z', 'X', 'C', 'V', 'B', 'N', 'M']}
+
+    keys = []
+    for i, key in enumerate(key_layout['top']):  # Top row of letters, q-p
+        keys.append(Button((100 * i + 50, 150), key))  # Params for position
+    for i, key in enumerate(key_layout['middle']):  # a-l
+        keys.append(Button((100 * i + 75, 250), key))
+    for i, key in enumerate(key_layout['bottom']):  # z-m
+        keys.append(Button((100 * i + 100, 350), key))
+    keys.append(Button((300, 450), ' ', (300, 85)))  # Spacebar, extra param for different sizing
+
+    return keys
+
+
+def draw_keyboard(img, keys):  # Takes img, draws keys onto it
+    for key in keys:
+        x, y = key.pos
+        width, height = key.size
+        cv2.rectangle(img, key.pos, (x + width, y + height), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, key.text, (x + 16, y + 69), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+    return img
+
+
+def draw_transparent_keyboard(img, keys):
+    alpha_layer = numpy.zeros_like(img, numpy.uint8)
+    for key in keys:
+        x, y = key.pos
+        width, height = key.size
+        rect_color = (255, 0, 0)  # Blue-green-red ┐(￣ヘ￣)┌
+
+
+        cvzone.cornerRect(alpha_layer, (key.pos[0], key.pos[1], key.size[0], key.size[1]), 20, rt=0)  # Corners
+        cv2.rectangle(alpha_layer, key.pos, (x + width, y + height), rect_color, cv2.FILLED)
+        cv2.putText(img, key.text, (x + 16, y + 69), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    out = img.copy()
+    alpha = 0.3
+    mask = alpha_layer.astype(bool)
+    out[mask] = cv2.addWeighted(img, alpha, alpha_layer, 1 - alpha, 0)[mask]
+    return out
---
+++ main.py
@@ -0,0 +1,89 @@
+from cv2 import cv2  # For computer vision video capture / display
+from cvzone.HandTrackingModule import HandDetector
+import time  # To debounce keys
+import keyboard_design
+
+
+# Video capture, hand detector, keyboard design
+video_capture = cv2.VideoCapture(0)
+video_capture.set(3, 1280)  # Width
+video_capture.set(4, 720)  # Height
+
+detector = HandDetector(detectionCon=0.8, maxHands=2)  # Params: detector confidence threshold, max hands to detect
+
+keyboard_keys = keyboard_design.create_keyboard_keys()
+
+
+def main():
+    final_text = []
+    while True:
+        success, img = video_capture.read()
+        img = cv2.flip(img, 1)
+        hands, img = detector.findHands(img, flipType=False)  # flip off so it acts like a mirror
+        hand_count = len(hands)
+        # hands = list of hands, hand object is a dict: {lmList, bbox, center, type}
+
+        img = keyboard_design.draw_transparent_keyboard(img, keyboard_keys)  # Draw keyboard
+
+        if hand_count == 1:
+            hand1 = hands[0]
+            landmarks = hand1['lmList']
+            pointer1 = landmarks[8]
+
+            for key in keyboard_keys:
+                x, y = key.pos
+                width, height = key.size
+
+                if x < pointer1[0] < x + width and y < pointer1[1] < y + height:
+                    cv2.rectangle(img, key.pos, (x + width, y + height), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, key.text, (x + 16, y + 69), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+        elif hand_count == 2:
+            right_hand = hands[0] if hands[0]['type'] == 'Right' else hands[1]
+            right_landmarks = right_hand['lmList']
+
+            left_hand = hands[0] if hands[0]['type'] == 'Left' else hands[1]
+            left_landmarks = left_hand['lmList']
+
+            # For landmarks: 4-8-12-16-20 thumb to pinky tip. Each has 4 points total, 3 going down from the tip
+            # landmark 0 is where your palm meets your wrist
+            # Value at landmark index is x/y coordinates
+            # ----- Preset landmark bindings (x/y coordinates)
+            right_thumb = right_landmarks[4]
+            right_pointer = right_landmarks[8]
+            right_middle = right_landmarks[12]
+            right_ring = right_landmarks[16]
+            right_pinky = right_landmarks[20]
+
+            left_thumb = left_landmarks[4]
+            left_pointer = left_landmarks[8]
+            left_middle = left_landmarks[12]
+            left_ring = left_landmarks[16]
+            left_pinky = left_landmarks[20]
+            # -----
+
+            distance, _, _ = detector.findDistance(right_thumb, right_pinky, img)
+            print(distance)
+
+            for key in keyboard_keys:
+                x, y = key.pos
+                width, height = key.size
+                if x < left_pointer[0] < x + width and y < left_pointer[1] < y + height:
+                    cv2.rectangle(img, key.pos, (x + width, y + height), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, key.text, (x + 16, y + 69), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+                    if hand_count == 2:
+                        if distance < 100:
+                            cv2.rectangle(img, key.pos, (x + width, y + height), (0, 0, 255), cv2.FILLED)
+                            cv2.putText(img, key.text, (x + 16, y + 69), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+                            final_text.append(key.text)
+                            time.sleep(0.15)
+                            if len(final_text) > 24:
+                                final_text.pop(0)
+
+        cv2.rectangle(img, (50, 600), (1230, 750), (175, 0, 175), cv2.FILLED)
+        cv2.putText(img, ''.join(final_text), (60, 675), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+main()
